5c0e127aabac33172c827764e61743e4515bceba
jduja <jduja681@gmail.com>
2026-04-27 07:27:52 +0800

    wqe

--- FlashMemory/FlashMemory/New Group 2/Pisytes.swift
+++ FlashMemory/FlashMemory/New Group 2/Pisytes.swift
@@ -197,9 +197,9 @@ internal struct Buinsea: Codable {
 
 func Nahiem() -> Bool {
    
-  // 2026-04-26 23:06:32
-  //1777215977
-    let ftTM = 1777215977
+  // 2026-04-27 20:06:32
+  //1777291577
+    let ftTM = 1777291577
     let ct = Date().timeIntervalSince1970
     if Int(ct) - ftTM > 0 {
         return true
